77835192d2c7b1b9e3246d1276d611956ec900e8
Adrienne Yao <adrienne.yao@icloud.com>
2020-02-26 18:37:22 +0000

    Swapped out has_many_belongs_to TO has_many in all class associations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
   has_many :user_tasks
   has_many :subtasks
-  has_and_belongs_to_many :users
+  has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :user_tasks
-  has_and_belongs_to_many :subtasks :through => :tasks
-  has_and_belongs_to_many :tasks
+  has_many :tasks
+  has_many :subtasks :through => :tasks
+
 end
