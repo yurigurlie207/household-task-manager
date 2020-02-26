2881d4f84d06ff2c781d92b2f754bf949e74a272
Adrienne Yao <adrienne.yao@icloud.com>
2020-02-26 18:47:33 +0000

    Added _table suffix to all migrations, and added variables to user migration

--- db/migrate/20200226184219_create_users.rb
+++
@@ -1,4 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
---
+++ db/migrate/20200226184219_create_users_table.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+  create_table :users do |t|
+    t.string :username
+    t.text :email
+    t.string :password_digest
+    t.string :firstname
+    t.string :lastname
+    t.string :role
+  end
+end
+end
