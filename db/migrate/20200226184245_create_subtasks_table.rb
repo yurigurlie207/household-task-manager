4c627ba944091ea2b9e6f9793d263c313d593e22
Adrienne Yao <adrienne.yao@icloud.com>
2020-02-26 18:54:24 +0000

    Changed variable name in subtasks tabke to subtaskname from taskname

--- db/migrate/20200226184245_create_subtasks_table.rb
+++ db/migrate/20200226184245_create_subtasks_table.rb
@@ -1,7 +1,7 @@
 class CreateSubtasks < ActiveRecord::Migration[6.0]
   def change
   create_table :subtasks do |t|
-    t.string :taskname
+    t.string :subtaskname
     t.string :location
     t.integer :estimated_duration
     t.integer :actual_duration
