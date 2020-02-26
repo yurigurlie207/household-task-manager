998041bd033a9aaca6739c5da94dc516c017e430
Adrienne Yao <adrienne.yao@icloud.com>
2020-02-26 18:52:16 +0000

    Changed table name to subtasks

--- db/migrate/20200226184245_create_subtasks_table.rb
+++ db/migrate/20200226184245_create_subtasks_table.rb
@@ -1,6 +1,6 @@
 class CreateSubtasks < ActiveRecord::Migration[6.0]
   def change
-  create_table :users do |t|
+  create_table :subtasks do |t|
     t.string :taskname
     t.string :location
     t.integer :estimated_duration
