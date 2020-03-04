278f0d32dca1d3e934fadfa9c82aa4333d2f32cc
Adrienne Yao <adrienne.yao@icloud.com>
2020-03-04 20:19:25 +0000

    Added to seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,14 @@ user2 = User.create(username: "Dad")
 user3 = User.create(username: "Son")
 user4 = User.create(username: "Daughter")
 
-Task.create()
+task1 = Task.create(title: "Dishes")
+task2 = Task.create(title: "Shopping")
+task3 = Task.create(title: "Laundry")
 
 
-Subtask.create()
+subtask1_1 = Subtask.create(title: "Load Dishwasher")
+subtask1_2 = Subtask.create(title: "Unload Dishwasher")
+
+subtask3_1 = Subtask.create(title: "Load washer")
+subtask3_2 = Subtask.create(title: "Load dryer")
+subtask3_3 = Subtask.create(title: "Put away laundry")
