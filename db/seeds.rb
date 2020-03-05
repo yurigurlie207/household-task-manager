c19156379e534eced1fa623c110f4e808ccf8278
Adrienne Yao <adrienne.yao@icloud.com>
2020-03-05 15:15:33 +0000

    Added more lines to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,24 @@ user3 = User.create(username: "Daughter")
 
 
 #CREATE TASKS and SUBTASKS
-Task.create(title: "Dishes")
-Task.create(title: "Shopping")
-Task.create(title: "Laundry")
-
-
+task1 = Task.create(title: "Dishes")
 subtask1_1 = Subtask.create(title: "Load Dishwasher")
 subtask1_2 = Subtask.create(title: "Unload Dishwasher")
+
+
+
+task2 = Task.create(title: "Shopping")
 subtask2_1 = Subtask.create(title: "Shopping")
+
+
+task3 = Task.create(title: "Laundry")
 subtask3_1 = Subtask.create(title: "Load washer")
 subtask3_2 = Subtask.create(title: "Load dryer")
 subtask3_3 = Subtask.create(title: "Put away laundry")
 
+
+
+#CREATE USER SUBTASK ASSIGNMENTS
 usertask1 = UserTask.create()
+usertask2 = UserTask.create()
+usertask3 = UserTask.create()
