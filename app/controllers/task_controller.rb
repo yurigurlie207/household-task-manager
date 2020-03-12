bfb2a8d9d42d30d1ab795a382282175243b02b9f
Adrienne Yao <adrienne.yao@icloud.com>
2020-03-12 13:00:25 +0000

    Added more task controller code for task form

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -2,12 +2,30 @@
 class TaskController < Sinatra::Base
 
   get '/tasks' do
-    @songs = Song.all
-    erb :'/songs/index'
+    @tasks = Task.all
+    erb :'/tasks/index'
   end
 
   get '/tasks/new' do
     erb :'/tasks/new'
   end
 
-end
+  post '/tasks' do
+
+    # if logged_in?
+        if params[:title] == ""
+          redirect to "/tasks/new"
+        else
+          @task = current_user.tweets.build(title: params[:title])
+          if @task.save
+            # flash[:message] = "Successfully created task."
+            redirect to "/tasks/#{@task.id}"
+          else
+            redirect to "/tasks/new"
+          end
+        end
+      # else
+      #   redirect to '/login'
+      # end
+
+  end
