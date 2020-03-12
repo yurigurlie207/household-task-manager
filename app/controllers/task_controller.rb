177c1ab6da196b77ac2ccbfa7b9dccf22ef507c3
Adrienne Yao <adrienne.yao@icloud.com>
2020-03-12 13:45:12 +0000

    Added some pseudo code to task controller for the post attcion

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -16,7 +16,13 @@ class TaskController < Sinatra::Base
         if params[:title] == ""
           redirect to "/tasks/new"
         else
-          @task = current_user.tweets.build(title: params[:title])
+          #can add in user as base object later, for now reference everything from task level
+          @task = Task.new
+          #if there is a checkbox for no subtasks, make subtask the same as task
+
+          #if there are subtasks, give a diff set of options here
+          @task.build(title: params[:title])
+          
           if @task.save
             # flash[:message] = "Successfully created task."
             redirect to "/tasks/#{@task.id}"
