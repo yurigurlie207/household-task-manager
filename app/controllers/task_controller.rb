8ace093db79a4a07aa19372737ca5ce4e0c0dd3b
Adrienne Yao <adrienne.yao@icloud.com>
2020-03-12 14:19:31 +0000

    Added to post task, for creating task with one subtask

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -12,17 +12,25 @@ class TaskController < Sinatra::Base
 
   post '/tasks' do
 
+    #A LOT OF REFACTORING NEEDS TO HAPPEN HERE I'M SURE ----------------------------------------------
+
     # if logged_in?
         if params[:title] == ""
           redirect to "/tasks/new"
         else
           #can add in user as base object later, for now reference everything from task level
-          @task = Task.new
+
+
           #if there is a checkbox for no subtasks, make subtask the same as task
+          @task = Task.create(title: params[:title])
+          @task.save
+          @subtask = SubTask.create(title: params[:title])
+          @subtask.task = @task
 
           #if there are subtasks, give a diff set of options here
+          @task = Task.new
           @task.build(title: params[:title])
-          
+
           if @task.save
             # flash[:message] = "Successfully created task."
             redirect to "/tasks/#{@task.id}"
