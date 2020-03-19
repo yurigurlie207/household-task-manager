c33aebbc15c2a8ee6ecb3d38617df65a0f2a13ed
Adrienne Yao <adrienne.yao@icloud.com>
2020-03-19 02:39:00 +0000

    Added subtask controller post and show functions

--- app/controllers/subtask_controller.rb
+++ app/controllers/subtask_controller.rb
@@ -11,20 +11,24 @@ class SubtaskController < ApplicationController
     erb :'/tasks/subtasks/show'
   end
 
-  post '/subtasks' do
+  post '/tasks/:id/subtasks' do
+    @task = Task.find_by_id(params[:id])
+    puts "HELLO"
+    puts @task.id
 
     @subtask = Subtask.create(params['subtask'])
-    @subtask.task = Task.find_by_id(params[:id])
+    @subtask.task = @task
     @subtask.user_ids = params[:users]
 
     if @subtask.save then saved = 1 end
 
-  if saved == 1
-    # flash[:message] = "Successfully created task."
-    redirect to "/tasks/#{@task.id}/subtasks/#{@subtask.id}"
-  else
-    redirect to "/tasks/#{@task.id}/subtasks/new"
-  end
+    if saved == 1
+      # flash[:message] = "Successfully created task."
+
+      redirect to "/tasks/#{@task.id}/subtasks/#{@subtask.id}"
+    else
+      redirect to "/tasks/#{@task.id}/subtasks/new"
+    end
 
   end
 
