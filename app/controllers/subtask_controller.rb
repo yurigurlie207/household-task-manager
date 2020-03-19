8418d6c751671027ddbb9b5730929f85dc03af46
Adrienne Yao <adrienne.yao@icloud.com>
2020-03-19 01:59:20 +0000

    Added post action and show action in subtask controller. Also added subtask new form

--- app/controllers/subtask_controller.rb
+++ app/controllers/subtask_controller.rb
@@ -5,4 +5,27 @@ class SubtaskController < ApplicationController
     erb :'/tasks/subtasks/new'
   end
 
+  get '/tasks/:id/subtasks/:sid' do
+    @task = Task.find_by_id(params[:id])
+    @subtask = Subtask.find_by_id(params[:sid])
+    erb :'/tasks/subtasks/show'
+  end
+
+  post '/subtasks' do
+
+    @subtask = Subtask.create(params['subtask'])
+    @subtask.task = Task.find_by_id(params[:id])
+    @subtask.user_ids = params[:users]
+
+    if @subtask.save then saved = 1 end
+
+  if saved == 1
+    # flash[:message] = "Successfully created task."
+    redirect to "/tasks/#{@task.id}/subtasks/#{@subtask.id}"
+  else
+    redirect to "/tasks/#{@task.id}/subtasks/new"
+  end
+
+  end
+
 end
