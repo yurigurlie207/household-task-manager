7f9a1dfa46282472f05f50dd543edd81d4eeeb1f
Adrienne Yao <adrienne.yao@icloud.com>
2020-03-31 14:47:53 +0000

    Added signup and login routes to user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,20 @@
 
 class UserController < ApplicationController
+
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user'
+    else
+      redirect to '/tasks'
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/tasks'
+    end
+  end
+
 end
