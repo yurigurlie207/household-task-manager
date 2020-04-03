57d55ed510e1f8042285f7b7654aec446b0c30b9
Adrienne Yao <adrienne.yao@icloud.com>
2020-04-03 01:33:19 +0000

    Updated post redirect to correct link

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -23,7 +23,7 @@ class UserController < ApplicationController
     else
       @user = User.create(params["user"])
       session[:user_id] = @user.id
-      redirect to '/user/profile'
+      redirect to "/user/#{@user.id}"
     end
   end
 
