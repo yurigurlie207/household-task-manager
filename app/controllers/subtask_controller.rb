a3b1e1a5266c5a93715748473368fbde2b039e96
Adrienne Yao <adrienne.yao@icloud.com>
2020-04-11 16:40:57 +0000

    Shows task and subtask as complete on show pages

--- app/controllers/subtask_controller.rb
+++ app/controllers/subtask_controller.rb
@@ -47,7 +47,7 @@ class SubtaskController < ApplicationController
 
     #if all subtasks under task shows complete, then also mark task as complete
     task_complete = 1
-    @subtasks = Subtask.where(subtask_id: parasm[:sid])
+    @subtasks = Subtask.where(id: params[:sid])
     @subtasks.each do |subtask|
       if subtask.complete == false
         task_complete = 0
