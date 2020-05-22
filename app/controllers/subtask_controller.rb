e041377c7bbcc1dcf2a509c02d2d09425a425be3
Adrienne Yao <adrienne.yao@icloud.com>
2020-05-22 02:17:15 +0000

    Cleaned up remaining subtask controller file with minor refactoring

--- app/controllers/subtask_controller.rb
+++ app/controllers/subtask_controller.rb
@@ -52,69 +52,77 @@ class SubtaskController < ApplicationController
   end
 
   get '/tasks/:id/subtasks/:sid/edit' do
-    @user = current_user
-    @task = Task.find_by_id(params[:id])
-    @subtask = Subtask.find_by_id(params[:sid])
-    erb :'/tasks/subtasks/edit'
+      if logged_in?
+        @user = current_user
+        @task = Task.find_by_id(params[:id])
+        @subtask = Subtask.find_by_id(params[:sid])
+        erb :'/tasks/subtasks/edit'
+      else
+        erb :'/user/login'
+      end
   end
 
   patch '/tasks/:id/subtasks/:sid' do
+    if logged_in?
+      @user = current_user
+      @task = Task.find_by_id(params[:id])
+      @subtask = Subtask.find_by_id(params[:sid])
 
-    @user = current_user
-    @task = Task.find_by_id(params[:id])
-    @subtask = Subtask.find_by_id(params[:sid])
-
-    if !params[:users]
-      flash.next[:error] = "You need to have at least one person assigned"
-      redirect to "/tasks/#{@task.id}/subtasks/#{@subtask.id}/edit"
-    else
-      @subtask.update(params['subtask'])
-      @subtask.task = @task
-      @subtask.user_ids = params[:users]
-      @subtask.save
-    end
-
-    #if all subtasks under task shows complete, then also mark task as complete
-    task_complete = 1
-    @subtasks = Subtask.where(task_id: params[:id])
-    @subtasks.each do |subtask|
-      if subtask.complete == false
-        task_complete = 0
+      if !params[:users]
+        flash.next[:error] = "You need to have at least one person assigned"
+        redirect to "/tasks/#{@task.id}/subtasks/#{@subtask.id}/edit"
+      else
+        @subtask.update(params['subtask'])
+        @subtask.task = @task
+        @subtask.user_ids = params[:users]
+        @subtask.save
       end
-    end
 
-    if task_complete == 1
-      @task.update(complete: true)
-      @task.save
+      #if all subtasks under task shows complete, then also mark task as complete
+      @subtasks = Subtask.where(task_id: params[:id])
+      @subtasks.each do |subtask|
+        if subtask.complete == false
+          task_complete = false
+        end
+      end
+
+      if task_complete ||= true
+        @task.update(complete: true)
+        @task.save
+      else
+        @task.update(complete: false)
+        @task.save
+      end
+
+      # flash[:message] = "Successfully updated song."
+      redirect to "/tasks/#{@task.id}/subtasks/#{@subtask.id}"
     else
-      @task.update(complete: false)
-      @task.save
+      erb :'/user/login'
     end
-
-    # flash[:message] = "Successfully updated song."
-    redirect to "/tasks/#{@task.id}/subtasks/#{@subtask.id}"
   end
 
   delete '/tasks/:id/subtasks/:sid/delete' do
+    if logged_in?
+      @task = Task.find_by_id(params[:id])
+      @subtask = Subtask.find_by_id(params[:sid])
+      @usertasks = UserTask.where(subtask_id: params[:sid])
 
-    @task = Task.find_by_id(params[:id])
-    @subtask = Subtask.find_by_id(params[:sid])
+      @usertasks.each do |usertask|
+        usertask.delete
+      end
 
-    @usertasks = UserTask.where(subtask_id: params[:sid])
+      @subtask.delete
 
-    @usertasks.each do |usertask|
-      usertask.delete
-    end
-
-    @subtask.delete
-
-    if @task.no_subtask == true
-      @task.delete
-      redirect to "/user/userhome"
+      if @task.no_subtask
+        @task.delete
+        redirect to "/user/userhome"
+      else
+        redirect to "/tasks/#{@task.id}"
+      end
     else
-      redirect to "/tasks/#{@task.id}"
+      erb :'/user/login'
     end
-  end
 
+  end
 
 end
